2ca893a7fdff6cd1a60d27511459ecb8c34ffeb8
Rubem Neto <rubemcorrea0@gmail.com>
2024-11-03 20:14:33 -0300

    feat: criando serviço para realizar a função de login

---
+++ src/services/login.ts
@@ -0,0 +1,18 @@
+import { LoginDto } from "../dtos/login.dto";
+import { NotFoundError } from "../errors/not-found-error";
+import { UnauthorizedError } from "../errors/unauthorized-error";
+import { AuthRepository } from "../repositories/auth-in-memory.repository";
+import { comparePasswords } from "../thirdparty/bcrypt";
+
+export class Login {
+  constructor(private readonly authRepository: AuthRepository) {}
+
+  async execute(account: LoginDto) {
+    const user = await this.authRepository.findByEmail(account.email);
+    if (!user) throw new NotFoundError("Usuário não encontrado!");
+
+    const isValid = await comparePasswords(account.password, user.password);
+    if (!isValid) throw new UnauthorizedError("Senha ou email incorreto!");
+    return user;
+  }
+}
