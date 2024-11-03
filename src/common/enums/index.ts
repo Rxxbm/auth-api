290bce83509b23acb305921a814b7fa45f20c905
Rubem Neto <rubemcorrea0@gmail.com>
2024-11-03 17:16:56 -0300

    feat: criando enums para as celulas, cargo e cargo de permissao

---
+++ src/common/enums/index.ts
@@ -0,0 +1,23 @@
+export enum Department {
+  MARKETING_COMUNICACAO = "MarketingComunicação",
+  MARKETING_PASSIVA = "MarketingPassiva",
+  GEP = "GestãoAdministrativaDePessoas",
+  PROJETOS_QUALIDADE = "ProjetosQualidade",
+  PROJETOS_COMPUTACAO = "ProjetosComputação",
+  PRESIDENCIA = "PRESIDENCIA",
+  ADMFIN = "AdministrativaFinanceiro",
+  COMERCIAL = "Comercial",
+}
+
+export enum EmployeeRole {
+  ADMIN = "Admin",
+  USER = "User",
+}
+
+export enum JobTitle {
+  ASSESSOR = "Assessor",
+  DIRETOR = "Diretor",
+  COORDENADOR = "Coordenador",
+  PRODUCT_OWNER = "ProductOwner",
+  PRESIDENTE = "Presidente",
+}
