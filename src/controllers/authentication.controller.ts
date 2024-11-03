762c11c83b41e8da3d1af2ad1ac953b7fc822113
Rubem Neto <rubemcorrea0@gmail.com>
2024-11-03 17:27:13 -0300

    feat: criando a rota de criação de conta

--- src/controllers/authentication.controller.ts
+++ src/controllers/authentication.controller.ts
@@ -1,10 +1,22 @@
-import { validate } from "class-validator";
 import { Controller } from "../decorators/http/controller";
 import { Get, Post } from "../decorators/http/methods";
 import { Middleware } from "../decorators/http/middleware";
-import { Request, Response, NextFunction } from "express";
-import { plainToInstance } from "class-transformer";
+import { Request, Response } from "express";
 import { CreateAccountDto } from "../dtos/authentication.dto";
+import { CreateAccount } from "../services/create-account.service";
+import { AuthInMemoryRepository } from "../repositories/auth-in-memory.repository";
+import { ValidateDto } from "../config/dto";
+import { hashPassword } from "../thirdparty/bcrypt";
+import { RouteResponse } from "../common/http-responses";
+import { ConflictError } from "../errors/conflict-error";
+import { EmployeeInMemoryRepository } from "../repositories/employee-in-memory.repository";
+
+const authInMemoryRepository = new AuthInMemoryRepository();
+const employeeInMemoryRepository = new EmployeeInMemoryRepository();
+const createAccountUseCase = new CreateAccount(
+  employeeInMemoryRepository,
+  authInMemoryRepository
+);
 
 @Controller("/auth")
 export class AuthController {
@@ -31,35 +43,26 @@ export class AuthController {
    *         description: Dados inválidos
    */
   @Post("/")
-  @Middleware((req: Request, res: Response, next: NextFunction) => {
-    console.log("Middleware for create-account executed!");
-    next();
-  })
-  public async createAccount(
-    request: Request,
-    response: Response,
-    next: NextFunction
-  ) {
-    const accountDto = plainToInstance(CreateAccountDto, request.body);
-
-    const errors = await validate(accountDto);
-    if (errors.length > 0) {
-      return response.status(400).json({
-        message: "Dados inválidos",
-        errors: errors
-          .map((error) => Object.values(error.constraints as unknown as any))
-          .flat(),
+  @Middleware(ValidateDto(CreateAccountDto)) // Por meio de um middleware valida se os dados da requisão está correto
+  public async createAccount(request: Request, response: Response) {
+    try {
+      //Realiza o HASH da senha
+      const hashedPassword = await hashPassword(request.body.password);
+      // Chama o caso de uso para criar a conta
+      const account = await createAccountUseCase.execute({
+        ...request.body,
+        password: hashedPassword,
       });
-    }
-    // Simulação de criação de conta
-    const newUser = {
-      id: Date.now(),
-      username: accountDto.name,
-    };
 
-    return response.status(201).json({
-      message: "Conta criada com sucesso",
-      user: newUser,
-    });
+      return RouteResponse.successCreated(response, account);
+    } catch (error) {
+      if (error instanceof Error) {
+        if (error instanceof ConflictError) {
+          return RouteResponse.conflictError(error.message, response);
+        }
+      } else {
+        return RouteResponse.serverError(error, response);
+      }
+    }
   }
 }
