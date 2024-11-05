071a2df86fae9595bad40694c118b90e4318c2ff
Rubem Neto <rubemcorrea0@gmail.com>
2024-11-05 12:15:40 -0300

    fix: corrigindo arquivo do repositorio em memoria do funcionario

---
+++ src/repositories/employee/employee-in-memory.repository.ts
@@ -0,0 +1,45 @@
+import { Repository } from "../../common/protocols/repository";
+import { Employee } from "../../entities/employee";
+
+export class EmployeeInMemoryRepository implements Repository<Employee> {
+  private users: Employee[] = [];
+
+  public async create(account: any) {
+    account.id = crypto.randomUUID();
+    account.created_at = new Date();
+    account.updated_at = new Date();
+    this.users.push(account);
+    return account;
+  }
+
+  public async list(): Promise<Employee[]> {
+    return this.users;
+  }
+
+  async delete(id: string): Promise<void> {
+    this.users = this.users.filter((user) => user.id !== id);
+  }
+
+  async findById(id: string): Promise<Employee | null> {
+    const user = this.users.find((user) => user.id === id);
+    return user ? user : null;
+  }
+
+  async findByAuthId(authId: string): Promise<Employee | undefined> {
+    const user = this.users.find((user) => user.auth_id === authId);
+    return user ? user : undefined;
+  }
+
+  async update(id: string, data: Partial<Employee>): Promise<Employee | null> {
+    const index = this.users.findIndex((user) => user.id === id);
+    if (index === -1) return null;
+    this.users[index] = { ...this.users[index], ...data };
+    return this.users[index];
+  }
+}
+
+export interface EmployeeRepository extends Repository<Employee> {
+  findByAuthId(authId: string): Promise<Employee | undefined>;
+}
+
+export const employeeRepository = new EmployeeInMemoryRepository();
